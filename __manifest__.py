85d9327755715a840cfb4e634a6657f6702bfc7c
Pablo Garcia <pagarciaa@gointerlogic.com>
2017-07-03 17:07:53 -0700

    Files

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': "warehouse_req-extended",
+    'summary': """
+        Extension de modulo de requerimientos para productos reparados.
+        """,
+    'description': """
+
+    	""",
+    'author': "Interlogic",
+    'website': "",
+    'category': 'Inventory',
+    'version': '0.1',
+    'depends': ['base', 'warehouse_req'],
+    'data': [
+        'views/warehouse_req-ext_views.xml',
+    ],
+}
---
+++ models/warehouse_req_ext.py
@@ -0,0 +1,18 @@
+from odoo import models, fields, api
+from odoo import _
+
+
+class WarehouseReqExt(models.Model):
+    _inherit = "warehouse.req"
+
+    product_type_req = fields.Selection(
+	selection=[
+        ('new', _('New')),
+        ('repaired', _('Repaired')),
+        ],
+        required=True,
+        default='new',
+	help=_("Products types to require"),
+	)
+
+    is_manufactured  = fields.Boolean()
